0d34e3097aae80bd5f95c5a8e18645d004022ed1
Elina <eli.lisenochka@gmail.com>
2016-03-28 18:30:50 +0300

    cat

---
+++ Animals/Cat.java
@@ -0,0 +1,12 @@
+package Animals;
+
+/**
+ * Created by Romych on 19.02.2016.
+ */
+public class Cat extends Animal {
+    public Cat(String name, float strength, int stamina, int courage, int speed, int jumpHeight) {
+        super(name, strength, stamina, courage, speed, jumpHeight);
+    }
+
+
+}
