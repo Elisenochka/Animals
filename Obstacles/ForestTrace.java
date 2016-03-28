6a99c1eca607ad00db7a54b1b51562c5b5e178e6
Elina <eli.lisenochka@gmail.com>
2016-03-28 18:30:37 +0300

    forest

---
+++ Obstacles/ForestTrace.java
@@ -0,0 +1,11 @@
+package Obstacles;
+
+/**
+ * Created by Romych on 19.02.2016.
+ */
+public class ForestTrace extends Obstacle{
+    int length;
+    public ForestTrace (int _length){
+        this.length = _length;
+    }
+}
