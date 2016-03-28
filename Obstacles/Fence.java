f40e85762ebf02f955436cb65ded18ea434ef8ee
Elina <eli.lisenochka@gmail.com>
2016-03-28 18:30:42 +0300

    fence

---
+++ Obstacles/Fence.java
@@ -0,0 +1,12 @@
+package Obstacles;
+
+/**
+ * Created by Romych on 19.02.2016.
+ */
+public class Fence {
+    int height;
+
+    public Fence(int _height){
+        this.height = _height;
+    }
+}
