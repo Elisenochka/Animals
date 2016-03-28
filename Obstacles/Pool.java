ca3ab0d13c16c7ab4b4a9dfd910eea43f24bd3e4
Elina <eli.lisenochka@gmail.com>
2016-03-28 18:30:09 +0300

    pool

---
+++ Obstacles/Pool.java
@@ -0,0 +1,17 @@
+package Obstacles;
+
+import Animals.Animal;
+
+/**
+ * Created by Romych on 19.02.2016.
+ */
+public class Pool extends Obstacle {
+    int depth;
+    int length;
+
+    public Pool(int _depth,int _length){
+        this.depth = _depth;
+        this.length = _length;
+    }
+
+}
